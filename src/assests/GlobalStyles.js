84f5afa8f95d195d57c7b075a36dfedc9243c93e
Natkn <larry.jeylor@gmail.com>
2025-04-23 22:12:42 +0300

    Initial commit

---
+++ src/assests/GlobalStyles.js
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+ * {
+    font-family: 'Montserrat', sans-serif;
+  }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  *:before,
+  *:after {
+    box-sizing: border-box;
+  }
+
+  a,
+  a:visited {
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  button,
+  ._btn {
+    cursor: pointer;
+    outline: none;
+  }
+
+  ul li {
+    list-style: none;
+  }
+
+
+  body, html {
+    height: 100%;
+    font-family: "Montserrat", Arial, Helvetica, sans-serif;
+    background: #F4F5F6;
+  
+  }
+
+`
---
+++ src/components/main/main.styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const MainBlock = styled.div`
+    height: 100vh;
+    background: #f4f5f6;
+    margin: 0;
+`
